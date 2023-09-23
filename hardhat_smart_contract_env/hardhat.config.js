745f4fe0265b13783f887fc10de2c835c4e13cf6
Vincent Palmerio <vpalmerio@protonmail.com>
2023-09-23 13:24:04 -0400

    Modified hardhat config for sepolia deployment, added dotenv

--- hardhat_smart_contract_env/hardhat.config.js
+++ hardhat_smart_contract_env/hardhat.config.js
@@ -1,6 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { SEPOLIA_PRIVATE_KEY } = process.env;
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia-rpc.scroll.io/` || "",
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
