0819c274bc495f31157c19a8ad61dd310f41745c
Soham Jog <jogsoham2003@gmail.com>
2023-09-23 23:36:13 -0400

    everything working

--- frontend/src/components/BuyComponent.js
+++ frontend/src/components/BuyComponent.js
@@ -1,62 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Order from './Order';
+import { useAccount, useContractRead, useContractWrite, useContractReads } from 'wagmi';
+import {SC_ADDRESS} from '../constants';
+import APIKeyEscrow from '../contracts/APIKeyEscrow.json';
 
 const apiUrl = "https://api.cloudnouns.com/v1/pfp";
 
-const availableAPIs = [
-  {
-    name: 'API 1',
-    duration: '30 days',
-    cost: '$50',
-    publicAddress: '0x123456789abcdef',
-    costPerHour: '$1.67',
-    isSeller: false,
-    image: 'https://api.cloudnouns.com/v1/pfp',
-    isUpForSale: true,
-  },
-  {
-    name: 'API 2',
-    duration: '15 days',
-    cost: '$30',
-    publicAddress: '0x987654321abcdef',
-    costPerHour: '$2.00',
-    isSeller: true,
-    image: 'https://api.cloudnouns.com/v1/pfp',
-    isUpForSale: true,
-  },
-  {
-    name: 'API 3',
-    duration: '30 days',
-    cost: '$50',
-    publicAddress: '0x123456789abcdef',
-    costPerHour: '$1.67',
-    isSeller: false,
-    image: 'https://api.cloudnouns.com/v1/pfp',
-    isUpForSale: true,
-  },
-  {
-    name: 'API 4',
-    duration: '15 days',
-    cost: '$30',
-    publicAddress: '0x987654321abcdef',
-    costPerHour: '$2.00',
-    isSeller: true,
-    image: 'https://api.cloudnouns.com/v1/pfp',
-    isUpForSale: true,
-  }
-  // Add more API objects as needed
-];
 
-function BuyComponent() {
+
+
+
+function BuyComponent({numberOfOrders}) {
+
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+
+  const { data, isError, isLoading } = useContractReads({
+      contracts: Array.from({ length: numberOfOrders }, (_, i) => ({
+          address: SC_ADDRESS,
+          abi: APIKeyEscrow.abi,
+          functionName: 'getOrder',
+          args: [i]
+      }))
+  });
+
+  const [apiData, setApiData] = useState([]);
+
+
+  useEffect(() => {
+    console.log(data);
+    setApiData([]);
+    for (let i = 0; i < data.length; i++) {
+      if (Number(data[i].result.price) == 0 || data[i].result.buyer != '0x0000000000000000000000000000000000000000') {
+        continue;
+      }
+      let api = {
+        name: "OpenAI API",
+        id: i,
+        duration: Number(data[i].result.duration),
+        cost: Number(data[i].result.price),
+        publicAddress: data[i].result.seller,
+        costPerHour: Number(data[i].result.price)/(Number(data[i].result.duration) * 24),
+        isSeller: 0,
+        image: 'https://api.cloudnouns.com/v1/pfp',
+        isUpForSale: true,
+      }
+      setApiData(apiData => [...apiData, api])
+    }
+    console.log(apiData)
+  }, []);
+
+
+  
+
+  
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       {/* Centered and blended-in menu tab */}
       <div className="bg-opacity-70 p-4 w-full h-full">
         <h1 className="text-2xl font-bold mb-4">Available APIs</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {availableAPIs.map((item, index) => (
+        {apiData.map((item, index) => (
           <Order 
             key={index}
+            id = {item.id}
             name = {item.name}
             duration={item.duration}
             cost={item.cost}
